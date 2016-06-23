17d8e69effa74c7da8c3a7c175b12c0fcfef36f7
Swelly <Surfswell17@gmail.com>
2016-06-23 12:27:14 -0400

    added youtube api key

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = "AIzaSyBq4PPm9bjwN203ZZcEE7CqseK8PuRad6U";
+
+const hostNode = document.querySelector('.container');
 // Create a new component
 // This should produce some HTML
 
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 }
 
+
 // Take this component's generated HTML
 // Place the HTML on the page (DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, hostNode);
