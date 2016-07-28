555df69aea1c2e3d33bec0b07e1d15f861fcf285
Swelly <Surfswell17@gmail.com>
2016-07-28 16:49:37 -0400

    youtube api next

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const API_KEY = "AIzaSyBq4PPm9bjwN203ZZcEE7CqseK8PuRad6U";
+import SearchBar from './components/search_bar';
 
+const API_KEY = "AIzaSyBq4PPm9bjwN203ZZcEE7CqseK8PuRad6U";
 const hostNode = document.querySelector('.container');
-// Create a new component
-// This should produce some HTML
+
 
 const App = () => {
-  return <div>Hi!</div>;
-}
+  return ( 
+    <div>
+      <SearchBar />
+    </div>
+  )
+};
 
-
-// Take this component's generated HTML
-// Place the HTML on the page (DOM)
 ReactDOM.render(<App />, hostNode);
